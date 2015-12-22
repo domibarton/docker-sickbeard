e3994bbe2cfcd5ea0670e8c6af46d18647cc3bbd
Dominique Barton <dbarton@confirm.ch>
2015-12-22 10:51:45 +0100

    BUGFIX: Updating git repository before checkout

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+echo "Updating SickBeard git repository..."
+git pull
+
 if [[ -n "${VERSION}" ]]
 then
     echo "Checking out SickBeard version '${VERSION}'..."
     git checkout ${VERSION}
 fi
-
-echo "Updating SickBeard..."
-git pull
 
 CONFIG=${CONFIG:-/datadir/config.ini}
 echo "Starting SickBeard with config '${CONFIG}'..."
